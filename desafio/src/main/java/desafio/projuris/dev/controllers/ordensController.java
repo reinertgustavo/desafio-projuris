855fb481d5578e4fcd8278b612cedca547277603
Gustavo Reinert <gustavo.reinert.do.amaral@gmail.com>
2021-02-03 12:06:45 -0300

    Beautify

--- desafio/src/main/java/desafio/projuris/dev/controllers/ordensController.java
+++ desafio/src/main/java/desafio/projuris/dev/controllers/ordensController.java
@@ -2,7 +2,6 @@ package desafio.projuris.dev.controllers;
 
 import java.net.URI;
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
